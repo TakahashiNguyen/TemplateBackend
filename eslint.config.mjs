a8e03be2d9923880eb9afe3a81942d7e592fac3a
Obscure6213 <83152264+ebd5028605c446562c8b39f69e2a6ac2204d5e@users.noreply.github.com>
2025-06-15 09:53:14 +0700

    Refactor code structure for improved readability and maintainability

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,34 +1,59 @@
-// @ts-check
-import eslint from '@eslint/js';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import js from '@eslint/js';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
+import json from '@eslint/json';
+import markdown from '@eslint/markdown';
+import { defineConfig } from 'eslint/config';
+import pluginJest from 'eslint-plugin-jest';
 
-export default tseslint.config(
-  {
-    ignores: ['eslint.config.mjs'],
-  },
-  eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  eslintPluginPrettierRecommended,
-  {
-    languageOptions: {
-      globals: {
-        ...globals.node,
-        ...globals.jest,
-      },
-      sourceType: 'commonjs',
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
-    },
-  },
-  {
-    rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
-    },
-  },
-);
+export default defineConfig([
+	{
+		files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+		plugins: { js },
+		extends: ['js/recommended'],
+	},
+	{ files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
+	{
+		files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+		languageOptions: { globals: globals.node },
+	},
+	tseslint.configs.recommended,
+	{
+		files: ['**/*.json'],
+		plugins: { json },
+		language: 'json/json',
+		extends: ['json/recommended'],
+	},
+	{
+		files: ['**/*.jsonc'],
+		plugins: { json },
+		language: 'json/jsonc',
+		extends: ['json/recommended'],
+	},
+	{
+		files: ['**/*.json5'],
+		plugins: { json },
+		language: 'json/json5',
+		extends: ['json/recommended'],
+	},
+	{
+		files: ['**/*.md'],
+		plugins: { markdown },
+		language: 'markdown/gfm',
+		extends: ['markdown/recommended'],
+	},
+	{
+		files: ['**/*.spec.js', '**/*.test.js'],
+		plugins: { jest: pluginJest },
+		languageOptions: {
+			globals: pluginJest.environments.globals.globals,
+		},
+		rules: {
+			'jest/no-disabled-tests': 'warn',
+			'jest/no-focused-tests': 'error',
+			'jest/no-identical-title': 'error',
+			'jest/prefer-to-have-length': 'warn',
+			'jest/valid-expect': 'error',
+		},
+	},
+]);
