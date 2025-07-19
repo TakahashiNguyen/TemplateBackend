2a81140cb80caf23e10767b60450233f5e7a7202
TakahashiNguyen <83152264+TakahashiNguyen@users.noreply.github.com>
2025-07-19 00:26:19 +0000

    Apply automatic changes

--- page/src/views/ChangePassword.vue
+++ page/src/views/ChangePassword.vue
@@ -1,29 +1,29 @@
 <!-- eslint-disable @typescript-eslint/no-unused-vars -->
 <template>
-  <FormContainerComp
-    btn-label="Confirm"
-    :btn-handle="handleRequest"
-    :alert="alert"
-    name="Change password"
-  >
-    <FormTextInputComp
-      name="Signature"
-      v-model="$route.params.signature"
-      :disable="true"
-      object="signature"
-      :alert="alert"
-      type="text"
-    ></FormTextInputComp>
-    <FormTextInputComp
-      name="New Password"
-      placeholder="Enter password"
-      icon="key_vertical"
-      v-model="input.password"
-      :alert="alert"
-      object="password"
-      type="password"
-    ></FormTextInputComp>
-  </FormContainerComp>
+	<FormContainerComp
+		btn-label="Confirm"
+		:btn-handle="handleRequest"
+		:alert="alert"
+		name="Change password"
+	>
+		<FormTextInputComp
+			name="Signature"
+			v-model="$route.params.signature"
+			:disable="true"
+			object="signature"
+			:alert="alert"
+			type="text"
+		></FormTextInputComp>
+		<FormTextInputComp
+			name="New Password"
+			placeholder="Enter password"
+			icon="key_vertical"
+			v-model="input.password"
+			:alert="alert"
+			object="password"
+			type="password"
+		></FormTextInputComp>
+	</FormContainerComp>
 </template>
 
 <script setup lang="ts">
@@ -33,7 +33,7 @@ import FormTextInputComp from '@/components/FormTextInputComp.vue';
 import { reactive } from 'vue';
 
 const input = reactive<IUserAuthentication>({
-    password: '',
-  }),
-  handleRequest = () => apiErrorHandler(action('change-password', input));
+		password: '',
+	}),
+	handleRequest = () => apiErrorHandler(action('change-password', input));
 </script>
