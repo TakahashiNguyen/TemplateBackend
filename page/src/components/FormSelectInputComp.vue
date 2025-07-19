05c3842f2adf53b6dbe4afd8a6afd70b724eac98
Obscure6213 <83152264+ebd5028605c446562c8b39f69e2a6ac2204d5e@users.noreply.github.com>
2025-07-19 00:27:44 +0000

    fix(web): minor issues

--- page/src/components/FormSelectInputComp.vue
+++ page/src/components/FormSelectInputComp.vue
@@ -1,50 +1,49 @@
 <template>
-  <div class="form-control mb-2">
-    <label class="label -mb-1.5">
-      <span class="label-text">{{ name }}</span>
-    </label>
-    <select
-      class="select select-bordered w-full"
-      v-model="model"
-      :class="{
-        'input-error': object === alert?.object && alert?.type === 'error',
-        'input-success': object === alert?.object && alert?.type === 'success',
-      }"
-    >
-      <option v-for="i in list" v-bind:key="i[0]" :value="i[1]">
-        {{ i[0].replace('_', ' ') }}
-      </option>
-    </select>
-    <label v-if="object === alert?.object" class="label -my-1.5">
-      <span
-        class="label-text-alt"
-        :class="{
-          'text-green-700': alert?.type === 'success',
-          'text-red-700': alert?.type === 'error',
-        }"
-      >
-        {{ alert?.message }}
-      </span>
-    </label>
-  </div>
-  <label v-if="subBtnClick" class="label -mt-3">
-    <a href="#" class="label-text-alt link link-hover" @click="subBtnClick">
-      <slot />
-    </a>
-  </label>
+	<div class="form-control mb-2">
+		<label class="label -mb-1.5">
+			<span class="label-text">{{ name }}</span>
+		</label>
+		<select
+			class="select select-bordered w-full"
+			v-model="model"
+			:class="{
+				'input-error': object === alert?.object && alert?.type === 'Fatal',
+				'input-success': object === alert?.object && alert?.type === 'Success',
+			}"
+		>
+			<option v-for="i in list" v-bind:key="i[0]" :value="i[1]">
+				{{ i[0].replace('_', ' ') }}
+			</option>
+		</select>
+		<label v-if="object === alert?.object" class="label -my-1.5">
+			<span
+				class="label-text-alt"
+				:class="{
+					'text-green-700': alert?.type === 'Success',
+					'text-red-700': alert?.type !== 'Success',
+				}"
+			>
+				{{ alert?.message }}
+			</span>
+		</label>
+	</div>
+	<label v-if="subBtnClick" class="label -mt-3">
+		<a href="#" class="label-text-alt link link-hover" @click="subBtnClick">
+			<slot />
+		</a>
+	</label>
 </template>
 
 <script setup lang="ts">
 import type { IAlert } from '@/error/interfaces';
-import type { IObject } from '@/error/types';
-
+import type { ErrorObject } from 'templatebackend';
 
 const model = defineModel();
 defineProps<{
-  name: string;
-  subBtnClick?: () => void;
-  object?: IObject;
-  alert?: IAlert;
-  list: Map<string, string>;
+	name: string;
+	subBtnClick?: () => void;
+	object?: ErrorObject;
+	alert?: IAlert;
+	list: Map<string, string>;
 }>();
 </script>
--- page/src/views/ChangePassword.vue
+++ page/src/views/ChangePassword.vue
@@ -1,39 +1,44 @@
 <!-- eslint-disable @typescript-eslint/no-unused-vars -->
 <template>
-  <FormContainerComp
-    btn-label="Confirm"
-    :btn-handle="handleRequest"
-    :alert="alert"
-    name="Change password"
-  >
-    <FormTextInputComp
-      name="Signature"
-      v-model="$route.params.signature"
-      :disable="true"
-      object="signature"
-      :alert="alert"
-      type="text"
-    ></FormTextInputComp>
-    <FormTextInputComp
-      name="New Password"
-      placeholder="Enter password"
-      icon="key_vertical"
-      v-model="input.password"
-      :alert="alert"
-      object="password"
-      type="password"
-    ></FormTextInputComp>
-  </FormContainerComp>
+	<FormContainerComp
+		btn-label="Confirm"
+		:button-handler="handleRequest"
+		:alert="alert"
+		name="Change password"
+	>
+		<FormTextInputComp
+			name="Signature"
+			v-model="$route.params.signature"
+			:disable="true"
+			object="signature"
+			:alert="alert"
+			type="text"
+		></FormTextInputComp>
+		<FormTextInputComp
+			name="New Password"
+			placeholder="Enter password"
+			icon="key_vertical"
+			v-model="input.authentication.password.value"
+			:alert="alert"
+			object="password"
+			type="password"
+		></FormTextInputComp>
+	</FormContainerComp>
 </template>
 
 <script setup lang="ts">
-import { action, alert, apiErrorHandler } from '@/auth.service';
+import { requester } from '@/api/functions';
 import FormContainerComp from '@/components/FormContainerComp.vue';
-import FormTextInputComp from '@/components/FormTextInputComp.vue';
+import FormTextInputComp from '@/components/TextInputComp.vue';
+import { apiErrorHandler, getAlert } from '@/error/functions';
+import { UserLoginDto } from 'templatebackend';
 import { reactive } from 'vue';
 
-const input = reactive<IUserAuthentication>({
-    password: '',
-  }),
-  handleRequest = () => apiErrorHandler(action('change-password', input));
+const alert = getAlert(),
+	input = reactive<UserLoginDto>({
+		email: '',
+		authentication: { type: 'password', password: { value: '' } },
+	}),
+	handleRequest = () =>
+		apiErrorHandler(requester('/change-password', input), alert);
 </script>
