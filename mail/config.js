f90eae122adf28e23519ec47ffb2536e94ace664
Obscure6213 <83152264+ebd5028605c446562c8b39f69e2a6ac2204d5e@users.noreply.github.com>
2025-07-19 00:37:35 +0000

    fix(mail): remove unnecessary statics

--- mail/config.js
+++ mail/config.js
@@ -11,11 +11,8 @@
 
 /** @type {import('@maizzle/framework').Config} */
 export default {
-  build: {
-    content: ['emails/**/*.html'],
-    static: {
-      source: ['images/**/*.*'],
-      destination: 'images',
-    },
-  },
-}
+	build: {
+		content: ['emails/**/*.html'],
+		static: {},
+	},
+};
