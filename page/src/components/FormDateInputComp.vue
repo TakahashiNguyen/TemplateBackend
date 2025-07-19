d36e6f5d881351a703b31edea045d2905da37317
TakahashiNguyen <83152264+TakahashiNguyen@users.noreply.github.com>
2025-07-19 00:30:43 +0000

    Apply automatic changes

--- page/src/components/FormDateInputComp.vue
+++ page/src/components/FormDateInputComp.vue
@@ -49,24 +49,23 @@ import type { ErrorObject } from 'templatebackend';
 import { onMounted, ref, watch } from 'vue';
 
 const props = defineProps<{
-    name: string;
-    object?: ErrorObject;
-    subBtnClick?: () => void;
-    alert?: IAlert;
-    icon?: string;
-    disable?: boolean;
-    modelValue: Date;
-  }>(),
-  datepickerRef = ref<HTMLInputElement | null>(null),
-  emit = defineEmits<{
-    (e: 'update:modelValue', value: Date): void;
-  }>();
-
+		name: string;
+		object?: ErrorObject;
+		subBtnClick?: () => void;
+		alert?: IAlert;
+		icon?: string;
+		disable?: boolean;
+		modelValue: Date;
+	}>(),
+	datepickerRef = ref<HTMLInputElement | null>(null),
+	emit = defineEmits<{
+		(e: 'update:modelValue', value: Date): void;
+	}>();
 
 const updateValue = (value: string) => {
-  const date = new Date(value);
-  if (date instanceof Date && !isNaN(date.getTime()))
-    emit('update:modelValue', new Date(value));
+	const date = new Date(value);
+	if (date instanceof Date && !isNaN(date.getTime()))
+		emit('update:modelValue', new Date(value));
 };
 
 watch(
