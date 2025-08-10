33c46b9069f35be03e38496e5dfb8ecf1505999d
Obscure6213 <83152264+ebd5028605c446562c8b39f69e2a6ac2204d5e@users.noreply.github.com>
2025-08-10 03:23:12 +0000

    chore(naming): change monorepos' name more suitable

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -7,7 +7,7 @@ const host = process.env.TAURI_DEV_HOST;
 
 // https://vite.dev/config/
 export default defineConfig({
-	root: '../page',
+	root: '../website',
 
 	plugins: [vue(), tailwindcss()],
 
@@ -35,7 +35,7 @@ export default defineConfig({
 
 	resolve: {
 		alias: {
-			'@': fileURLToPath(new URL('../page/src', import.meta.url)),
+			'@': fileURLToPath(new URL('../website/src', import.meta.url)),
 		},
 	},
 });
